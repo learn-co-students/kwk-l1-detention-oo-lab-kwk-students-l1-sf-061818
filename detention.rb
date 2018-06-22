2661e32f6fb259165bc6a642b7657a3527dac8d9
Paikea Melcher <paikeamelcher@proofschool.org>
2018-06-22 17:38:17 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,23 @@
 #Code your detention class here
+class Detention
+  attr_accessor :location, :time, :grumpy_teacher, :activity, :students
+  def initialize()
+    @activity = "collective punishment"
+    @students = []
+  end
+  
+  def add_student(student)
+    @students.push(student)
+  end
+  
+  def remove_student(student)
+    @students.delete(student)
+  end
+end
+
+# @@varname
+# def initialize
+#   @@varname
+# def self.varname
+#   @@varname
+# end
